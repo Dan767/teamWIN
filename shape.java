178e1759c4c4109aefa0f5abae345fa0176b91cb
Ryan Jensen <rrjensen@ualberta.ca>
2019-01-29 19:15:55 -0700

    Modified Shape appropriately

--- shape.java
+++ shape.java
@@ -2,9 +2,11 @@ public abstract class Shape () {
   public int x;
   public int y;
   String color;
-  Shape (int x, int y, String color) {
+  public int z;
+  Shape (int x, int y, int z, String color) {
     this.x = x;
     this.y = y;
+    this.z = z;
     this.color = color;
   }
 }
